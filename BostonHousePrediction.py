ff02286fc8786b3bc658ea8e87d530b35d81be5f
Jonathan Thimesch <JRThimesch@gmail.com>
2019-10-02 19:32:37 -0500

    Update BostonHousePrediction.py

--- BostonHousePrediction.py
+++ BostonHousePrediction.py
@@ -2,6 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_boston
+
 boston = pd.read_csv('data/boston_housing.data', header=None, delimiter=' ')
 print(boston)
